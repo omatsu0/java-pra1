c70f0b31dc9db6f672fa58baeca9926a9acd34b2
“sss-o2” <your@example.com>
2020-11-22 01:20:37 +0900

    lesson4

--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ package myProject;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+
+		String msg;
+		msg = "Hello World Again!";
+		System.out.println(msg);
 
 	}
 
