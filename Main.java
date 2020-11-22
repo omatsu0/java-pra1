a1ad57e72e89c2ebcf799adc20511a2710f91be3
“sss-o2” <your@example.com>
2020-11-22 19:12:39 +0900

    lesson6

--- Main.java
+++ Main.java
@@ -4,18 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		char a = 'a';
+//
 
-		int x = 10;
-		long y = 5555555555L;
-
-		double d= 23423.344;
-		float f = 32.33F;
-
-		boolean flag = true;
-
-		String msg = "Hello Wo\nrld Again\t";
-		System.out.println(msg);
+		String s;
+		s = "Hello" + "world";
+		System.out.println(s);
 
 	}
 
