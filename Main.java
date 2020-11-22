42db95e52ea51b46d8df154b622ec0228a74d5ec
“sss-o2” <your@example.com>
2020-11-22 19:47:27 +0900

    lesson10

--- Main.java
+++ Main.java
@@ -5,22 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 
 		String signal="green";
-		
-		switch (signal) {
-		  case "red":
-			  System.out.println("stop!");
-			  break;
-		  case "blue":
-		  case "green":
-			  System.out.println("go!");
-			  break;
-		  case "yellow":
-			  System.out.println("caution");
-			  break;
-		  default:
-			  System.out.println("wrong signal");
-			  break;
-		}
+
+		int i=100;
+
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<10);
     }
 
 }
