13b9c10b7a7a3e2010625eab3ce32013316458c4
“sss-o2” <your@example.com>
2020-11-23 02:33:49 +0900

    lesson15

--- Main.java
+++ Main.java
@@ -2,13 +2,14 @@ package myProject;
 
 public class Main {
 
+	public static String sayHi(String name) {
+		return "Hi!" + name;
+	}
 	public static void main(String[] args) {
+			String msg=sayHi("Steve");
 
-		int[] sales = {400,200,300};
+			System.out.println(msg);
 
-		for (int sale : sales) {
-			System.out.println(sale);
-		}
 
 
     }
