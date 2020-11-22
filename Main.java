648ad837fe114a715cbb6a10a08e64fe5ec41e69
“sss-o2” <your@example.com>
2020-11-22 19:40:08 +0900

    lesson9

--- Main.java
+++ Main.java
@@ -4,11 +4,23 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int score =70;
-
-		String msg= score > 80 ? "Great!" : "so...so...!";
-		System.out.println(msg);
-
-	}
+		String signal="green";
+		
+		switch (signal) {
+		  case "red":
+			  System.out.println("stop!");
+			  break;
+		  case "blue":
+		  case "green":
+			  System.out.println("go!");
+			  break;
+		  case "yellow":
+			  System.out.println("caution");
+			  break;
+		  default:
+			  System.out.println("wrong signal");
+			  break;
+		}
+    }
 
 }
