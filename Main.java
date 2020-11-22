3db18a7fd9b2257abb486c47e5142df201d3faa1
“sss-o2” <your@example.com>
2020-11-23 02:11:17 +0900

    lesson14

--- Main.java
+++ Main.java
@@ -4,11 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int[] sales = {100,200,300};
+		int[] sales = {400,200,300};
 
-		System.out.println(sales[1]);
-		sales[1]=1000;
-		System.out.println(sales[1]);
+		for (int sale : sales) {
+			System.out.println(sale);
+		}
+
 
     }
 
