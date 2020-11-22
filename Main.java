584eefdebdde6c66fca1c449e0a8d631392f1bb6
“sss-o2” <your@example.com>
2020-11-23 02:38:26 +0900

    lesson16

--- Main.java
+++ Main.java
@@ -2,16 +2,16 @@ package myProject;
 
 public class Main {
 
-	public static String sayHi(String name) {
-		return "Hi!" + name;
+	public static void sayHi(String name) {
+		System.out.println("Hi!" + name);
 	}
+	public static void sayHi() {
+		System.out.println("Hi! Nobody!");
+	}
+
 	public static void main(String[] args) {
-			String msg=sayHi("Steve");
-
-			System.out.println(msg);
-
-
-
+		sayHi();
+		sayHi("Steve");
     }
 
 }
